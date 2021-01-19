f0fc825afa6fad3bf37e845dd9310b639c904b27
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-01-19 11:42:49 -0300

    Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
 
 
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+/*
+var count;
+for(count=1; count <= 5; count ++){
+	alert(count);
+};
+/*
+
+/*
+var count = 0;
+while (count <= 5){
+	console.log(count);
+	alert(count);
+	count++;
+};
+/*
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+	alert("maior de idade");
+}else{
+	alert("menor de idade");
+};
+*/
+
+/*
 var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
 console.log(frutas);
 alert(frutas[1].nome);
+*/
 
-/*var fruta = {nome: "maça", cor:"vermelha"}
+/*
+var fruta = {nome: "maça", cor:"vermelha"}
 console.log(fruta.nome);
-alert(fruta.cor);*/
+alert(fruta.cor);
+*/
 
-/*var lista = ["maça", "pera", "laranja"];
+/*
+var lista = ["maça", "pera", "laranja"];
 lista.push("uva");
 //lista.pop();
 console.log(lista[0]);
 console.log(lista.toString()[0]);
-console.log(lista.join(" | "));*/
+console.log(lista.join(" | "));
+*/
 
-/*var n1 = 5
+/*
+var n1 = 5
 var n2 = 3
 var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos");
@@ -23,4 +61,5 @@ var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
-//alert(frase.replace("Japão", "Brasil"));*/
+//alert(frase.replace("Japão", "Brasil"));
+*/
