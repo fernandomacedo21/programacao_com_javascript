3dd83acde3170fa79a7f35834ba6c4d2de18ecd5
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-01-19 11:10:27 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 
 
-var nome = "Fernando Macedo"
-var n1 = 5
+var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+//lista.pop();
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" | "));*/
+
+/*var n1 = 5
 var n2 = 3
 var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos");
@@ -9,4 +23,4 @@ var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
-//alert(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));*/
